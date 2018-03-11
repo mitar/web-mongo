c102507923e85d5a54e10ab802b6535e0f2961d8
Mitar <mitar.git@tnode.com>
2018-03-11 13:28:34 -0700

    Adding Vue.

--- src/client/app.js
+++ src/client/app.js
@@ -1,5 +1,14 @@
 import {MongoClient} from 'mongodb';
-import assert from 'assert';
+import Vue from 'vue';
+
+import App from './app.vue';
+
+new Vue({
+  el: '#app',
+  render: function (createElement) {
+    return createElement(App);
+  }
+});
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -14,9 +23,6 @@ const insertDocuments = function(db, callback) {
   collection.insertMany([
     {a : 1}, {a : 2}, {a : 3}
   ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
     console.log("Inserted 3 documents into the collection");
     callback(result);
   });
@@ -27,7 +33,6 @@ const findDocuments = function(db, callback) {
   const collection = db.collection('documents');
   // Find some documents
   collection.find({}).toArray(function(err, docs) {
-    assert.equal(err, null);
     console.log("Found the following records");
     console.log(docs);
     callback(docs);
@@ -36,7 +41,6 @@ const findDocuments = function(db, callback) {
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
   console.log("Connected correctly to server");
 
   const db = client.db(dbName);
---
+++ src/client/app.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="red">{{msg}}</h1>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'Hello world!'
+      }
+    }
+  }
+</script>
+
+<style>
+  .red {
+    color: #f00;
+  }
+</style>
